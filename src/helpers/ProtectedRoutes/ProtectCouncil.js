6c235d1e13105978b2c4912c93737625799e9692
daniatvm <daniatvm@gmail.com>
2020-06-11 12:39:59 -0600

    Ahora se redirecciona dependiendo del rol del usuario

--- src/helpers/ProtectedRoutes/ProtectCouncil.js
+++ src/helpers/ProtectedRoutes/ProtectCouncil.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import roles from "../roles";
+import auth from "../auth";
 
 export const ProtectCouncil = ({
     component: Component,
@@ -12,6 +13,11 @@ export const ProtectCouncil = ({
             render={props => {
                 if (roles.isCouncilModifier()) {
                     return <Component {...props} />;
+                }
+                if (auth.isAuthenticated()) {
+                    return (
+                        <Redirect to='/consejos' />
+                    );
                 } else {
                     return (
                         <Redirect to="/" />
