65b21832e72f691e929751efc9874a4290f852e5
daniatvm <daniatvm@gmail.com>
2020-07-30 12:06:22 -0600

    bug de fechas solucionado

--- src/helpers/dates.js
+++ src/helpers/dates.js
@@ -7,10 +7,10 @@ export const getTodaysDate = () => {
 }
 
 export const requestDay = () => {
-  let today = new Date();
-  const dd = String(today.getDate()).padStart(2, '0');;
-  const mm = String(today.getMonth() + 1).padStart(2, '0');;
-  const yyyy = today.getFullYear();
-  const due = parseInt(dd) + 3;
-  return yyyy + '-' + mm + '-' + due.toString();
+  let due = new Date();
+  due.setDate(due.getDate() + 3)
+  const dd = String(due.getDate()).padStart(2, '0');;
+  const mm = String(due.getMonth() + 1).padStart(2, '0');;
+  const yyyy = due.getFullYear();
+  return yyyy + '-' + mm + '-' + dd;
 }
